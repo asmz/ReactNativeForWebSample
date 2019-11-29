e43c87577267deb4abf0408987f3c2728f420a41
asmz <land.front.high.field@gmail.com>
2019-11-29 12:32:38 +0900

    Add react-native-web

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const appDirectory = path.resolve(__dirname, './');
+
+module.exports = {
+  entry: path.resolve(appDirectory, 'index.web.js'),  // エントリとなるJS
+  output: {
+    // 最終的に webroot/bundle.web.js に出力
+    filename: 'bundle.web.js',
+    path: path.resolve(appDirectory, 'webroot')
+  },
+  module: {
+    rules: [
+      {
+        // ts-loaderを利用する設定
+        test: /\.(tsx?)$/,
+        exclude: [ '/node_modules/' ],
+        use: 'ts-loader',
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.web.js', '.js', '.ts', '.web.ts', '.tsx', '.web.tsx' ],
+    alias: {
+      // POINT:
+      // Webpackビルドでは'react-native'は'react-native-web'として名前解決
+      'react-native': 'react-native-web'
+    }
+  }
+}
