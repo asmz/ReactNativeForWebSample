d2ece972612b9b93db831c82b2001671e08a04de
asmz <land.front.high.field@gmail.com>
2019-12-08 00:45:41 +0900

    Fix

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,3 +1,11 @@
-self.addEventListener('install', (event) => {
-  console.log('installed.')
+self.addEventListener('install', event => {
+  console.log('installed.');
+});
+
+self.addEventListener('activate', event => {
+  console.log('activated');
+});
+
+self.addEventListener('fetch', event => {
+  console.log('fetched.');
 });
